888249677a9e60ebb72d7a6ca15c0630134134c0
Naoto Satoh <stardustforever012@gmail.com>
2017-10-20 19:40:53 +0900

    add Hmac sample code

---
+++ src/main/java/com/example/Hmac.java
@@ -0,0 +1,36 @@
+package com.example;
+
+
+import com.fressets.wallet.utils.ApiRequestHandler;
+import com.fressets.wallet.utils.RequestParam;
+
+
+public class Hmac {
+    public static void main(String[] args) {
+        String accsess_key = "accsesskey1";
+        String secret_key = "akhjla";
+        int nonce = 122121;
+        String payload = "test payload"; // json.toString();
+
+        //リクエストパラメータのオブジェクト作成
+        RequestParam requestParam = new RequestParam(accsess_key, nonce, payload);
+
+        //この形式のJSONをリクエストBDOYまたはGETで送りたい。
+        ApiRequestHandler apiHandler = new ApiRequestHandler(requestParam, secret_key);
+        String sig = apiHandler.createHmacSignature();
+        System.out.println(sig);
+
+//        String secret_key2 = "aa";
+
+        //署名の認証
+        RequestParam requestParam2 = new RequestParam(accsess_key, nonce, payload, sig);
+        ApiRequestHandler apiHandler2 = new ApiRequestHandler(requestParam2, secret_key);
+//        ApiRequestHandler apiHandler2 = new ApiRequestHandler(requestParam2, secret_key2, "aaa");
+
+        if(apiHandler2.isValidRequest()){
+            System.out.println("valid signature");
+        }else{
+            System.out.println("invalid signature");
+        }
+    }
+}
